09ea9ff2ffe24f087f2431aa6d0d9dea3d6a00b1
mrtmeeseeks <tao.disha@gmail.com>
2022-06-12 21:41:22 +0300

    Latest changes

--- src/pages/integrate/Integrate.tsx
+++ src/pages/integrate/Integrate.tsx
@@ -2,6 +2,22 @@ import Stepper from '@components/Stepper';
 import { Step } from '@components/Stepper/model';
 import SelectMarketStep from './SelectMarketStep';
 
+// useEffect(() => {
+//   console.log(formState.isValid, 'formState');
+//   props.setActions(() => {
+//     return (
+//       <>
+//         <Button variant="contained" disabled={!formState.isValid} disableElevation onClick={previous}>
+//           Previous {props?.stepper?.currentStep}
+//         </Button>
+//         <Button variant="contained" disableElevation onClick={onSubmit}>
+//           Next
+//         </Button>
+//       </>
+//     );
+//   });
+// }, [props?.stepper?.currentStep, formState.isValid]);
+
 const steps: Step[] = [
   {
     component: SelectMarketStep,
