2323980552343a7f181fd001af688d045aab42b0
mrtmeeseeks <tao.disha@gmail.com>
2022-07-05 02:19:20 +0300

    Changed commitment messages

--- src/pages/integrate/misc.ts
+++ src/pages/integrate/misc.ts
@@ -42,25 +42,19 @@ export const MarketTemplates = [
 export const CommitmentMessages = (value: number) => {
   switch (+value) {
     case 1:
-      return `I got 99 problems, and a community ain't one`;
+      return `Just lurking 👀`;
     case 2:
-      return 'Billie Jean is not my lover.';
     case 3:
-      return `They think I'm hiding in the shadows. But I am the shadows.`;
     case 4:
-      return 'Eight or higher, bro.';
+      return 'gm gm 😪';
     case 5:
-      return `Yes, no, maybe, I don't know. Can you repeat the question?`;
     case 6:
-      return 'Pivot!';
     case 7:
-      return 'You Jump, I Jump, Jack.';
+      return 'Trusted seed 🌱';
     case 8:
-      return 'You have my sword. And you have my bow. And my ax';
     case 9:
-      return 'I’m a Mandalorian.';
     case 10:
-      return '“After all this time?" "Always...”';
+      return `It's a Soulbound ⛓️`;
     default:
       return `Minimum Commitment Level for new Members.`;
   }
