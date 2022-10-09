1924aa34d522f85dbd20d25f8389804f7ad697c5
mrtmeeseeks <tao.disha@gmail.com>
2022-10-09 14:57:47 +0300

    Implemented fetching of network config from backend endpoint

--- src/api/environment.ts
+++ src/api/environment.ts
@@ -1,4 +1,3 @@
-import { NetworkConfig } from '@store/model';
 import { envionmentGenerator } from 'sw-web-shared';
 
 export enum EnvMode {
@@ -11,15 +10,8 @@ export const swEnvVariables = {
   env: 'REACT_APP_NODE_ENV',
   apiUrl: 'REACT_APP_API_URL',
 
-  // Aut networks
-  networks: 'REACT_APP_NETWORKS',
-  networkNames: 'REACT_APP_NETWORK_NAMES',
-  rpcUrls: 'REACT_APP_RPC_URLS',
-  chainIds: 'REACT_APP_CHAIN_IDS',
-  blockExplorerUrls: 'REACT_APP_BLOCK_EXPLORER_URLS',
-  registryAddresses: 'REACT_APP_DAO_REGISTRY_ADDRESSES',
-  autIDAddresses: 'REACT_APP_AUT_ID_ADDRESSES',
-  daoTypesAddresses: 'REACT_APP_DAO_TYPES_ADDRESSES',
+  // Networks
+  networkEnv: 'REACT_APP_NETWORK_ENV',
 
   // NFT storage
   nftStorageKey: 'REACT_APP_NFT_STORAGE_KEY',
@@ -27,25 +19,3 @@ export const swEnvVariables = {
 };
 
 export const environment: typeof swEnvVariables = envionmentGenerator(swEnvVariables);
-
-export const getNetworkVariables = (name: string): NetworkConfig => {
-  const index = environment.networks.split(',').findIndex((r) => r.trim().toLowerCase() === name.trim().toLowerCase());
-  const autIdAddress = environment.autIDAddresses.split(',')[index];
-  const daoTypeAddress = environment.daoTypesAddresses.split(',')[index];
-  const registryAddress = environment.registryAddresses.split(',')[index];
-  const rpcUrls = environment.rpcUrls.split(',')[index].split('|');
-  const blockExplorerUrls = environment.blockExplorerUrls.split(',')[index].split('|');
-  const chainId = environment.chainIds.split(',')[index];
-
-  return {
-    autIdAddress,
-    registryAddress,
-    daoTypeAddress,
-    network: {
-      name,
-      rpcUrls,
-      chainId: Number(chainId),
-      blockExplorerUrls,
-    },
-  } as NetworkConfig;
-};
