b4cf712eb19bd40d8664b2ab5d8d0aef16d61c48
antant <ant.iv.ge@gmail.com>
2023-01-09 15:52:58 +0200

    colored with offwhite

---
+++ src/theme/commitment-slider-styles.tsx
@@ -0,0 +1,169 @@
+import { FormHelperText } from "@components/Fields";
+import { useTheme } from "@emotion/react";
+import {
+  Typography,
+  Slider,
+  SliderProps,
+  styled,
+  PaletteColor,
+  Theme,
+  Breakpoint,
+  ComponentsOverrides,
+  ComponentsProps,
+  ComponentsVariants
+} from "@mui/material";
+import { CommitmentMessages } from "@utils/misc";
+import { pxToRem } from "@utils/text-size";
+import { FieldErrors } from "react-hook-form";
+
+export function CommitmentMessage({ value, children = null }) {
+  const message = CommitmentMessages(value);
+  return (
+    <Typography
+      color="white"
+      whiteSpace="nowrap"
+      align="left"
+      component="span"
+      fontSize={pxToRem(18)}
+      sx={{ display: "flex", mb: "4px" }}
+    >
+      {message}
+    </Typography>
+  );
+}
+
+const fontSize = {
+  xs: "16px",
+  sm: "16px",
+  md: "16px",
+  lg: "16px",
+  xxl: "24px"
+};
+
+const errorTypes = {
+  min: "Min 1 commitment level!"
+};
+
+interface AutSliderProps {
+  value: any;
+  sliderProps: SliderProps;
+  name: string;
+  errors: any;
+  sx?: any;
+}
+
+const SliderWrapper = styled("div")({
+  position: "relative"
+});
+
+export const AutCommitmentSlider = ({
+  value,
+  name,
+  errors,
+  sx,
+  sliderProps,
+  ...props
+}: AutSliderProps) => {
+  const theme = useTheme();
+
+  return (
+    <SliderWrapper sx={sx}>
+      <CommitmentMessage value={value} />
+      <div style={{ position: "relative" }}>
+        <Slider {...sliderProps} />
+      </div>
+      <div
+        style={{
+          marginTop: "-3px",
+          display: "flex",
+          justifyContent: "flex-end"
+        }}
+      >
+        <FormHelperText
+          errorTypes={errorTypes}
+          value={value}
+          name={name}
+          errors={errors}
+        >
+          <Typography color="white" variant="body1">
+            No worries, you’ll be able to change this later.
+          </Typography>
+        </FormHelperText>
+      </div>
+    </SliderWrapper>
+  );
+};
+
+const addOpacity = (color, opacity) => {
+  const colorWithOpacity = color.slice(0, 1) + opacity + color.slice(1);
+  return colorWithOpacity;
+};
+
+const generateColors = (color: PaletteColor) => ({
+  color: color.dark,
+  ".MuiSlider-mark": {
+    borderColor: color.light
+  },
+  ".MuiSlider-track": {
+    background: color.light
+  },
+  ".MuiSlider-thumb": {
+    background: color.main
+  },
+  ".MuiSlider-thumb:hover": {
+    boxShadow: `0px 0px 0px 0px`
+  },
+  ".MuiSlider-thumb.Mui-focusVisible": {
+    boxShadow: `0px 0px 0px 0px`
+  }
+});
+
+export default (theme: Theme) =>
+  ({
+    ...theme.components.MuiSelect,
+    styleOverrides: {
+      root: {
+        "&.MuiSlider-colorPrimary": generateColors(theme.palette.offWhite),
+        width: pxToRem(600),
+        height: pxToRem(65),
+        borderRadius: "0",
+        borderWidth: "2px",
+        borderStyle: "solid",
+        padding: "0",
+
+        'span[data-index="10"].MuiSlider-mark': {
+          display: "none"
+        },
+        'span[data-index="0"].MuiSlider-mark': {
+          display: "none"
+        },
+        ".MuiSlider-thumb": {
+          width: "45px",
+          height: "45px"
+        },
+        ".MuiSlider-mark": {
+          background: "transparent",
+          width: "5px",
+          height: "5px",
+          borderRadius: "50%",
+          borderStyle: "solid",
+          borderWidth: "1px",
+
+          "&.MuiSlider-markActive": {
+            border: "none"
+          }
+        },
+        ".MuiSlider-track": {
+          borderRight: "0"
+        },
+
+        ".MuiSlider-rail": {
+          opacity: "0"
+        }
+      }
+    } as ComponentsOverrides<Theme>["MuiSlider"]
+  } as {
+    defaultProps?: ComponentsProps["MuiSlider"];
+    styleOverrides?: ComponentsOverrides<Theme>["MuiSlider"];
+    variants?: ComponentsVariants["MuiSlider"];
+  });
