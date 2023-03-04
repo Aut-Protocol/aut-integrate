ee1397c3cdda3e80df569522c97cb0150ca1f7b4
mrtmeeseeks <tao.disha@gmail.com>
2023-03-04 19:31:09 +0200

    Added initial cache request

--- src/api/registry.api.ts
+++ src/api/registry.api.ts
@@ -1,6 +1,8 @@
 import AutSDK, { SDKContractGenericResponse } from "@aut-labs-private/sdk";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { Community } from "./community.model";
+import axios from "axios";
+import { environment } from "./environment";
 
 export const isMemberOfDao = createAsyncThunk(
   "membership-checker/is-member",
@@ -20,10 +22,23 @@ export const isMemberOfDao = createAsyncThunk(
   }
 );
 
+export const updateCache = async (cache: any): Promise<any> => {
+  const res = await axios.post(
+    `${environment.apiUrl}/autID/cache/addOrUpdateCache`,
+    cache
+  );
+  return res?.data || null;
+};
+
 export const createCommunity = createAsyncThunk(
   "integrate/create/community",
   async (
-    requestBody: { metadata: Community; contractType: number; daoAddr: string },
+    requestBody: {
+      metadata: Community;
+      contractType: number;
+      daoAddr: string;
+      userAddress: string;
+    },
     { rejectWithValue }
   ) => {
     const sdk = AutSDK.getInstance();
@@ -46,6 +61,28 @@ export const createCommunity = createAsyncThunk(
       );
     }
     if (response?.isSuccess) {
+      try {
+        await updateCache({
+          address: requestBody.userAddress,
+          daoAddress: requestBody.daoAddr,
+          list: [
+            {
+              phase: 1,
+              status: 1
+            },
+            {
+              phase: 2,
+              status: 0
+            },
+            {
+              phase: 3,
+              status: 0
+            }
+          ]
+        });
+      } catch (error) {
+        // handle error
+      }
       return response.data;
     }
     return rejectWithValue(response?.errorMessage);
