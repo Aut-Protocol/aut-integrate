44b64c896f74fd406a4be604ada124b6ab9f9bda
mrtmeeseeks <tao.disha@gmail.com>
2024-09-07 19:57:05 +0300

    removed a commented line

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,6 @@ function App() {
   const location = useLocation();
   const ps = useRef<HTMLElement>();
   const scrollRestorationState = useSelector(ScrollRestorationState);
-  // const isAllowListed = useSelector(IsAllowListed);
 
   useEffect(() => {
     getAppConfig()
